41fc9cb0315114b7491c67bd6221f411b180d5ae
solsider <defremov11@gmail.com>
2026-03-30 16:35:26 +0300

    Delete budgettracker directory

--- budgettracker/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_login import LoginManager
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
-# Общие расширения вынесены отдельно, чтобы избежать циклических импортов.
-db = SQLAlchemy()
-login_manager = LoginManager()
-migrate = Migrate()
